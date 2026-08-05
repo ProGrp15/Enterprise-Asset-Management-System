66b6f95c8e88dd1b767b77fdaa9dd39cc9b065a1
Rohit Ravijit Teke <rohitteke4056@gmail.com>
2026-08-05 13:26:55 +0530

    dotNet/CompanyService directory

--- dotNet/CompanyService/DTOs/DepartmentDto.cs
+++
@@ -1,8 +0,0 @@
-namespace CompanyService.DTOs
-{
-    public class DepartmentDto
-    {
-        public string Name { get; set; } = string.Empty;
-        public string? Description { get; set; }
-    }
-}
